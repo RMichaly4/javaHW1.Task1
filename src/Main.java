892ce349650030086790508723beeb1e9d4f9c54
RMichaly4 <100rm500@gmail.com>
2018-09-12 16:07:00 +0300

    it works!

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ import java.io.*;
 
 public class Main {
 
-    // specify file below
+    // specify file to read from below
     static String FILE = "C:\\=DATA\\#WORK\\TestScripts\\javaHW1.Task1.v02\\src\\Logs.txt";
 
     // specify string to find below
@@ -10,40 +10,45 @@ public class Main {
 
 
     public static void main(String[] args) {
-        System.out.println("\n" + "Searching string: " + "'" + STRING_TO_FIND +"'"+ " in the file: " + "'" + FILE + "'"+ "\n" + "***************************************" + "\n");
-//      int matchCount = 0;
-
+        System.out.println("\n" + "Searching string: " + "'" + STRING_TO_FIND +"'"+ " in the file: " + "'" + FILE + "'"+ "\n" + "====================================");
 
         try{
             FileInputStream fstream = new FileInputStream(FILE);
             BufferedReader br = new BufferedReader(new InputStreamReader(fstream));
-            String strLine;
-            int lineNum = 0;
-            int matchCount = 0;
+            // specify file to write to below
+            FileWriter writer = new FileWriter("C:\\=DATA\\#WORK\\TestScripts\\javaHW1.Task1.v02\\src\\errorLogs.txt", true);
+        int lineNum = 0;
+        int matchCount = 0;
+        String strLine;
             while ((strLine = br.readLine()) != null){
                 //System.out.println(strLine);
                 lineNum++;
 
                 if(strLine.indexOf(STRING_TO_FIND) > -1) {
-                    if (matchCount == 0) {System.out.println("'StringToFind' was found on line(s)"+ "\n");
+                    if (matchCount == 0) {System.out.println("'"+ STRING_TO_FIND + " was found on line(s)"+ "\n");
                     }
-                    System.out.println("Str. " + lineNum +": " + strLine + "\n" );
+                    String textToWrite = "Str. " + lineNum +": " + strLine + "\n";
+                    System.out.println(textToWrite);
+
+                    writer.write(textToWrite);
+                    writer.flush();
                     matchCount++;
                 }
-
 
             }
 
             System.out.println("TOTAL " + matchCount+ " LINES FOUND");
-            if (matchCount == 0) {System.out.println("\n" + "!!! 'StringToFind' not found !!!");
+            if (matchCount == 0) {System.out.println("\n" + "### String: '" + STRING_TO_FIND +"'" +" not found! ###");
             }
-
         }
+
+
 
         catch(IOException ex){
 
             System.out.println(ex.getMessage());
         }
+
     }
 
 
